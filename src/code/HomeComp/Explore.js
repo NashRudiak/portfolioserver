884ba5fc36ddec7b4f02da7b0825861cc4d91b7a
Nash Rudiak <nashrudiak@Nashs-MBP.cogeco.local>
2025-02-23 16:15:30 -0500

    Added animations to text font

--- src/code/HomeComp/Explore.js
+++ src/code/HomeComp/Explore.js
@@ -3,35 +3,42 @@ import { NavLink } from 'react-router-dom';
 import { useInView } from "react-intersection-observer";
 
 function Explore() {
-    const [proj1, inView1] = useInView({ threshold: 0.5 });
-    const [proj2, inView2] = useInView({ threshold: 0.5 });
-    const [proj3, inView3] = useInView({ threshold: 0.5 });
+    const [suggestcap, inViewCap] = useInView({ threshold: 0.5 });
+    const [proj1, inView1] = useInView({ threshold: 0.1 });
+    const [proj2, inView2] = useInView({ threshold: 0.1 });
+    const [proj3, inView3] = useInView({ threshold: 0.1 });
     return(
         <div class="section">
             <div class="fillbar" id="right" />
             <div class="playspace projectoverview" id="left">
-                <div class="suggestbox">
+                <div class="suggestbox" ref={suggestcap}>
                     <div class="titlefont emphtext">Explore</div>
-                    <div class="textbox">See the various projects Nash has collaborated/contributed to</div>
+                    <div class={inViewCap ? "textbox suggestcaption textanim" : " suggestcaption textbox suggesthide"}>See the various projects Nash has contributed towards</div>
                     <NavLink to="/projects"><button class="button">View Projects</button></NavLink>
                 </div>
                 <div class="projectbox">
                     <ul>
-                        <li ref={proj1}>
-                            <div class={inView1 ? "" : "hidden"}>
-                                proj1test
-                            </div>
-                        </li>
-                        <li ref={proj2}>
-                            <div class={inView2 ? "" : "hidden"}>
-                                proj2hehe
-                            </div>
-                        </li>
-                        <li ref={proj3}>
-                            <div class={inView3 ? "" : "hidden"}>
-                                proj3
-                            </div>
-                        </li>
+                        <div ref={proj1}>
+                            <li class={inView1 ? "slidein" : "hidden"}>
+                                <div class={inView1 ? "textlist" : "hidden"}>
+                                    p
+                                </div>
+                            </li>
+                        </div>
+                        <div ref={proj2}>
+                            <li class={inView2 ? "slidein" : "hidden"}>
+                                <div class={inView2 ? "textlist" : "hidden"}>
+                                    proj2hehe
+                                </div>
+                            </li>
+                        </div>
+                        <div ref={proj3}>
+                            <li class={inView3 ? "slidein" : "hidden"}>
+                                <div class={inView3 ? "textlist" : "hidden"}>
+                                    proj3
+                                </div>
+                            </li>
+                        </div>
                     </ul>
                 </div>
             </div>
