044fae21a165154fa48fc724ed45379e9a50ed37
Nash Rudiak <nashrudiak@Nashs-MBP.cogeco.local>
2025-04-01 17:20:23 -0400

    Added extra files

---
+++ src/code/Footer.js
@@ -0,0 +1,15 @@
+import '../stylesheets/App.css';
+
+function FooterLinks() {
+
+}
+
+function Footer() {
+    return(
+        <div class="Footer">
+            
+        </div>
+    );
+}
+
+export default Footer;
